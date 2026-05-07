46dfa9b6160f5d1b489242150cb4ed6bfedd6a69
AnetZ <23308060610457@cetis61.edu.mx>
2026-05-06 19:27:48 -0600

    cambios

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,9 +1,10 @@
 from models.UserModel import UsuarioModel
+
 class AuthController:
     def __init__(self):
         self.usuario_model = UsuarioModel()
 
-    def login(self, email, password):
+    def login(self, email, password, page): 
         try:
             user_db = self.usuario_model.validar_login(email, password)
 
@@ -14,14 +15,36 @@ class AuthController:
                 "id_usuario": user_db["id_usuario"],
                 "nombre": user_db["nombre"],
                 "apellido": user_db["apellido"],
-                "email": user_db["email"]
+                "email": user_db["email"],
+                "ultimo_acceso": user_db.get("ultimo_acceso", "Reciente"),
+                "foto_perfil": user_db.get("foto_perfil", "assets/predeterminado.png") 
             }
+
+            self.guardar_perfil_en_historial(page, user)
 
             return user, "Login exitoso"
 
         except Exception as e:
             return None, f"Error en login: {str(e)}"
     
+    def guardar_perfil_en_historial(self, page, user_data):
+        """Lógica para recordar la cuenta en el dispositivo"""
+        try:
+            cuentas = page.client_storage.get("perfiles_activos") or []
+            
+            nuevo_perfil = {
+                "id": user_data['id_usuario'],
+                "nombre": user_data['nombre'],
+                "email": user_data['email'],
+                "fecha": user_data['ultimo_acceso'],
+                "foto": user_data['foto_perfil']
+            }
+            
+            if not any(p['id'] == nuevo_perfil['id'] for p in cuentas):
+                cuentas.append(nuevo_perfil)
+        except Exception as e:
+            print(f"No se pudo guardar el perfil local: {e}")
+
     def registrar(self, usuario_data):
         try:
             if self.usuario_model.email_existe(usuario_data.email):
@@ -35,20 +58,7 @@ class AuthController:
                 
         except Exception as e:
             return False, f"Error en registro: {str(e)}"
-        
+
 def login_exitoso(page, user_data): 
-    cuentas = page.client_storage.get("perfiles_activos") or []
-    
-    nuevo_perfil = {
-        "id": user_data['id_usuario'],
-        "nombre": user_data['nombre'],
-        "fecha": user_data.get('ultimo_acceso', 'Reciente')
-    }
-    
-    # Evitar duplicados
-    if not any(p['id'] == nuevo_perfil['id'] for p in cuentas):
-        cuentas.append(nuevo_perfil)
-        page.client_storage.set("perfiles_activos", cuentas)
-    
-    # Después de guardar, usualmente querrás navegar al dashboard
+    page.user_data = user_data
     page.go("/dashboard")
